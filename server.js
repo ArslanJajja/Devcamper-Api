faa80a7babe04a4ca9740ac331771276d842864d
Muhammad Arslan <arslanjajja1@gmail.com>
2021-07-24 13:36:21 +0500

    dev-camper api

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express=require('express')
 const dotenv=require('dotenv')
 const morgan=require('morgan')
+const colors=require('colors')
+const connectDB = require('./config/db')
 // Route Files
 const bootcamps=require('./Routes/bootcamps')
-
 // Load env vars
 dotenv.config({path:'./config/config.env'})
+// Connect to database
+connectDB()
 const app=express()
 // Dev logging middleware
 if(process.env.NODE_ENV='development'){
@@ -15,4 +18,9 @@ if(process.env.NODE_ENV='development'){
 app.use('/api/v1/bootcamps',bootcamps)
 const PORT=process.env.PORT ||5000
 
-app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode in ${PORT}`))
+const server=app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode in ${PORT}`.yellow.bold))
+
+process.on('unhandledRejection',(err,promise)=>{
+console.log(`Error: ${err.message}`.red);
+server.close(()=>process.exit(1))
+})
